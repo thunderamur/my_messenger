c46346c94d21d43410ecb87edbf050c288a441b9
thunder <thunder-amur@yandex.ru>
2017-11-07 16:29:12 +0900

    Remove jim tests from test_client.py

--- test_client.py
+++ test_client.py
@@ -1,6 +1,5 @@
-import time
-
 from client import MessengerClient
+
 
 class TestMessengerClient(object):
     def setup(self):
@@ -11,13 +10,4 @@ class TestMessengerClient(object):
         assert self.mc.user == {
             'account_name': 'name',
             'status': 'status'
-        }
-
-    def test_jim_presence(self):
-        p = self.mc.jim_presence()
-        assert p['action'] == 'presence'
-
-    def test_jim_quit(self):
-        assert self.mc.jim_quit() == {
-            'action': 'quit'
         }
