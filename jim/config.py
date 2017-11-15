9d2029080200418b4f3a2cfca52f91125408af8d
thunder <thunder-amur@yandex.ru>
2017-11-15 11:02:28 +0900

    Add response code and comment some constants

--- jim/config.py
+++ jim/config.py
@@ -1,26 +1,46 @@
-"""Константы для jim протокола, настройки"""
-# Ключи
-ACTION = 'action'
-TIME = 'time'
-USER = 'user'
-ACCOUNT_NAME = 'account_name'
-RESPONSE = 'response'
-ERROR = 'error'
+"""
+JIM protocol config file.
+"""
 
-# Значения
-PRESENCE = 'presence'
+# ************************************
+# * Explicit is better than implicit *
+# ************************************
+# # Keys
+# ACTION = 'action'
+# TIME = 'time'
+# TYPE = 'type'
+# USER = 'user'
+# ACCOUNT_NAME = 'account_name'
+# PASSWORD = 'password'
+# TO = 'to'
+# FROM = 'from'
+# MESSAGE = 'message'
+# ROOM = 'room'
+# RESPONSE = 'response'
+# ALERT = 'alert'
+# ERROR = 'error'
+#
+# # Values
+# PRESENCE = 'presence'
+# PROBE = 'probe'
+# MSG = 'msg'
+# QUIT = 'quit'
+# JOIN = 'join'
+# LEAVE = 'leave'
+# AUTHENTICATE = 'authenticate'
+# STATUS = 'status'  # also is key for USER
 
-# Коды ответов (будут дополняться)
+# Response codes
 BASIC_NOTICE = 100
+IMPORTANT_NOTICE = 101
 OK = 200
+CREATED = 201
 ACCEPTED = 202
-WRONG_REQUEST = 400  # неправильный запрос/json объект
+WRONG_REQUEST = 400  # or JSON object
+NOT_AUTHORIZED = 401
+WRONG_LOGIN_OR_PASSWORD = 402
+FORBIDDEN = 403
+NOT_FOUND = 404
+CONFLICT = 409
+GONE = 410
 SERVER_ERROR = 500
-
-RESPONSE_CODES = {
-    100: 'basic_notice',
-    200: 'ok',
-    202: 'accepted',
-    400: 'wrong_request',
-    500: 'server_error'
-}
