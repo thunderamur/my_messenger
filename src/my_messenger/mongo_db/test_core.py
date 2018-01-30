c469d45fec9bb82a816724e2947af7cd4ebe6f80
thunder <thunder-amur@yandex.ru>
2018-01-30 11:38:51 +0900

    Create test

---
+++ src/my_messenger/mongo_db/test_core.py
@@ -0,0 +1,21 @@
+from .core import MongoRepo
+
+
+class TestMongoRepo:
+    def setup(self):
+        self.mongo_repo = MongoRepo()
+
+    def test_push_and_pop(self):
+        self.mongo_repo.push('test_from', 'test_to', 'test_message')
+        self.mongo_repo.push('test_from', 'test_to', 'test_message_2')
+        result1 = None
+        result2 = None
+        result3 = None
+        for item in self.mongo_repo.db.messages.find({'from': 'test_from', 'to': 'test_to'}):
+            result1 = item
+        for item in self.mongo_repo.pop('test_from', 'test_to'):
+            result2 = item
+        for item in self.mongo_repo.db.messages.find({'from': 'test_from', 'to': 'test_to'}):
+            result3 = item
+        assert result1 == result2
+        assert result3 != result2
